54589b6a971c971691e328f43cc16dc41da1b6cf
onkar <onkar@DESKTOP-AVQKCQC>
2022-11-13 17:16:16 +0530

    reconfigured routes in separate files

--- src/MyBud.ApiGateway/Program.cs
+++ src/MyBud.ApiGateway/Program.cs
@@ -1,14 +1,21 @@
+using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var routes = "Routes";
+
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
                     .AddJsonFile("appsettings.json", true, true)
                     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
-                    .AddJsonFile("ocelot.json")
+                    .AddOcelot(routes, builder.Environment)
                     .AddEnvironmentVariables();
 builder.Services.AddOcelot();
+builder.Configuration.AddOcelotWithSwaggerSupport(options =>
+{
+    options.Folder = routes;
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
