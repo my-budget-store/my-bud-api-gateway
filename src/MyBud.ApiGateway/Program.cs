17f8c7aa0a1be4db10e168b975cc384ec7337303
onkar <onkar@DESKTOP-AVQKCQC>
2022-11-17 19:00:36 +0530

    implement auth

--- src/MyBud.ApiGateway/Program.cs
+++ src/MyBud.ApiGateway/Program.cs
@@ -1,21 +1,43 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var routes = "Routes";
 
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                    .AddJsonFile("appsettings.json", true, true)
-                    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
-                    .AddOcelot(routes, builder.Environment)
-                    .AddEnvironmentVariables();
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddOcelot(routes, builder.Environment)
+    .AddEnvironmentVariables();
+builder.Configuration
+    .AddOcelotWithSwaggerSupport(options => { options.Folder = routes; });
 builder.Services.AddOcelot();
-builder.Configuration.AddOcelotWithSwaggerSupport(options =>
-{
-    options.Folder = routes;
-});
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = false;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = JwtRegisteredClaimNames.Sub,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]))
+        };
+    });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,6 +45,8 @@ builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -32,7 +56,6 @@ if (app.Environment.IsDevelopment())
         options.PathToSwaggerGenerator = "/swagger/docs";
     }).UseOcelot().Wait();
 }
-
 app.UseHttpsRedirection();
 
 app.Run();
